95efa9204da012acc682812f44254d1c6f79281c
pruzanov <pruzanov@oicr.on.ca>
2021-11-12 15:45:40 -0500

    Making RT tests more robust

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 cd $1
 
-# HS file  - need o remove first few lines which may change between runs
-# INS file - need o remove first few lines which may change between runs
-# INS.pdf file, need to remove date info from the start of the file
+# txt file  - need o remove first few lines which may change between runs
 
-echo "HS files:"
-for f in *.recal.HS;do echo $f;tail -n+12 $f | md5sum;done
+echo ".txt files:"
+for f in *.txt;do echo $f;tail -n+12 $f | md5sum;done
 
-echo "INS files:"
-for f in *.recal.INS;do echo $f;tail -n+12 $f | md5sum;done
-
-echo "pdf files:"
-for f in *.pdf;do echo $f;tail -n+11 $f | md5sum;done
