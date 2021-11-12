230a7a56b5abaaff772bfc555bb83fcc1fbe26ea
pruzanov <pruzanov@oicr.on.ca>
2021-11-12 15:12:49 -0500

    Making RT tests more robust

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,4 @@
+## Unreleased - 2021-11-11
+[GP-2880](https://jira.oicr.on.ca/browse/GP-2880) Making RT tests more robust
 ## 2.0.3 - 2021-06-22
 - Changing types for bed file inputs File -> String
--- tests/calculate.sh
+++ tests/calculate.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 cd $1
-ls | sed 's/.*\.//' | sort | uniq -c
+
+# HS file  - need o remove first few lines which may change between runs
+# INS file - need o remove first few lines which may change between runs
+# INS.pdf file, need to remove date info from the start of the file
+
+echo "HS files:"
+for f in *.recal.HS;do echo $f;tail -n+12 $f | md5sum;done
+
+echo "INS files:"
+for f in *.recal.INS;do echo $f;tail -n+12 $f | md5sum;done
+
+echo "pdf files:"
+for f in *.pdf;do echo $f;tail -n+11 $f | md5sum;done
